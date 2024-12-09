47e2867f99080da10701b4a85ab3c44264f5b54c
VincentMugondora <vinmugondora@gmail.com>
2024-12-09 13:29:52 +0200

    @latest

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({extended:false}))
 
 // CORS setup
 const corsOptions = {
-  origin: "http://localhost:5173", 
+  origin: "https://mugo-plumbing-solutions-client.vercel.app", 
   methods: "GET,POST,PUT,DELETE",
   credentials: true,
 };
